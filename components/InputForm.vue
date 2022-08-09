6e3970e1c8f8fade8606a5051ea8c89e93978b10
ChibaTomoki <michiamo.t1@gmail.com>
2022-08-09 21:04:31 +0900

    InputForm.vue対応

--- components/InputForm.vue
+++ components/InputForm.vue
@@ -1,3 +1,96 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import axios, { AxiosResponse, AxiosError } from 'axios'
+import { ItemPerson, Item } from '../model'
+import StringInput from './StringInput.vue'
+import PositiveIntInput from './PositiveIntInput.vue'
+
+interface Data {
+  tempItem: Item
+  valid: boolean
+}
+
+interface Props {
+  initItem: Item
+}
+
+interface Emits {
+  (e: 'posted-item'): void
+  (e: 'get-all-items'): void
+}
+
+const props = defineProps<Props>()
+const emits = defineEmits<Emits>()
+
+const tempItem = reactive({
+  name: props.initItem.name,
+  persons: props.initItem.persons,
+  note: props.initItem.note,
+})
+const valid = ref(true)
+const formRef = ref<any>(null)
+
+const updateName = (newName: string): void => {
+  tempItem.name = newName
+}
+
+const updatePaid = (newPaid: number, index: number): void => {
+  if (tempItem.persons[index]) tempItem.persons[index].paid = newPaid
+  initializeToPay()
+}
+
+const updateToPay = (newToPay: number, index: number): void => {
+  if (tempItem.persons[index]) tempItem.persons[index].toPay = newToPay
+}
+
+const updateNote = (newNote: string): void => {
+  tempItem.note = newNote
+}
+const initializeToPay = (): void => {
+  const paidSum: number = tempItem.persons.reduce((sum: number, person: ItemPerson) => sum + person.paid, 0)
+  const roundedAliquot: number = Math.round(paidSum / tempItem.persons.length)
+  let remainder: number = paidSum - roundedAliquot * tempItem.persons.length
+
+  tempItem.persons.forEach((person: ItemPerson) => {
+    let addition: number = 0
+    if (remainder > 0) {
+      addition++
+      remainder--
+    } else if (remainder < 0) {
+      addition--
+      remainder++
+    }
+    person.toPay = roundedAliquot + addition
+  })
+}
+
+const initializeTempItem = (): void => {
+  tempItem.name = ''
+  tempItem.persons.forEach((person) => {
+    person.paid = 0
+    person.toPay = 0
+  })
+  tempItem.note = ''
+}
+
+const postItem = (): void => {
+  const isOk: boolean = formRef.value.validate()
+  if (!isOk) return
+
+  axios
+    .post('/api', tempItem)
+    .then((response: AxiosResponse<Item>) => {
+      console.log(response)
+      emits('get-all-items')
+    })
+    .then(() => {
+      initializeTempItem()
+      emits('posted-item')
+    })
+    .catch((error: AxiosError) => console.log(error))
+}
+</script>
+
 <template>
   <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="postItem">
     <StringInput
@@ -24,129 +117,5 @@
     </template>
     <StringInput :value="tempItem.note" label="メモ" @update-value="updateNote($event)" />
     <v-btn type="submit" @click="validate"> 追加 </v-btn>
-    <!-- <b-alert :show="doneFirstSubmit && hasNameEmptyError" variant="danger" class="my-1" style="width: 14rem">
-      購入品を入力してください
-    </b-alert> -->
-    <!-- <b-alert :show="doneFirstSubmit && hasTotalAmountError" variant="danger" class="my-1" style="width: 14rem">
-      払った額と払う額の合計を同じにしてください
-    </b-alert> -->
   </v-form>
 </template>
-
-<script lang="ts">
-import Vue, { PropType } from 'vue'
-import { ItemPerson, Item } from '../model'
-
-interface Data {
-  tempItem: Item
-  valid: boolean
-  doneFirstSubmit: boolean
-}
-
-export default Vue.extend({
-  name: 'InputFormComponent',
-  components: {
-    StringInput: () => import('./StringInput.vue'),
-    PositiveIntInput: () => import('./PositiveIntInput.vue'),
-  },
-  props: {
-    initItem: {
-      type: Object as PropType<Item>,
-      default: () => ({ name: '', persons: [{ index: 0, name: '', paid: 0, toPay: 0 }], note: '' }),
-    },
-  },
-  data: (): Data => ({
-    tempItem: {
-      name: '',
-      persons: [],
-      note: '',
-    },
-    valid: true,
-    doneFirstSubmit: false,
-  }),
-  computed: {
-    hasTotalAmountError(): boolean {
-      return this.getTotalPaid() !== this.getTotalToPay()
-    },
-    hasNameEmptyError(): boolean {
-      return this.tempItem.name === ''
-    },
-  },
-  created(): void {
-    this.tempItem = JSON.parse(JSON.stringify(this.initItem))
-  },
-  methods: {
-    updateName(newName: string): void {
-      this.$set(this.tempItem, 'name', newName)
-    },
-    updatePaid(newPaid: number, index: number): void {
-      this.$set(this.tempItem.persons[index], 'paid', newPaid)
-      this.initializeToPay()
-    },
-    updateToPay(newToPay: number, index: number): void {
-      this.$set(this.tempItem.persons[index], 'toPay', newToPay)
-    },
-    updateNote(newNote: string): void {
-      this.$set(this.tempItem, 'note', newNote)
-    },
-    initializeToPay(): void {
-      const paidSum = this.tempItem.persons.reduce((sum, person) => sum + person.paid, 0)
-      const roundedAliquot = Math.round(paidSum / this.tempItem.persons.length)
-      let remainder: number = paidSum - roundedAliquot * this.tempItem.persons.length
-
-      this.tempItem.persons.forEach((person) => {
-        let addition: number = 0
-        if (remainder > 0) {
-          addition++
-          remainder--
-        } else if (remainder < 0) {
-          addition--
-          remainder++
-        }
-        this.$set(person, 'toPay', roundedAliquot + addition)
-      })
-    },
-    initializeTempItem(): void {
-      this.$set(this.tempItem, 'name', '')
-      this.tempItem.persons.forEach((person) => {
-        this.$set(person, 'paid', 0)
-        this.$set(person, 'toPay', 0)
-      })
-      this.$set(this.tempItem, 'note', '')
-      this.doneFirstSubmit = false
-    },
-    getTotalPaid(): number {
-      return this.tempItem.persons.reduce(
-        (totalPaid: number, current: ItemPerson): number => totalPaid + current.paid,
-        0
-      )
-    },
-    getTotalToPay(): number {
-      return this.tempItem.persons.reduce(
-        (totalToPay: number, current: ItemPerson): number => totalToPay + current.toPay,
-        0
-      )
-    },
-    postItem(): void {
-      this.doneFirstSubmit = true
-      if (this.hasTotalAmountError || this.hasNameEmptyError) return
-
-      this.$axios
-        .post('/api', this.tempItem)
-        .then((response) => {
-          console.log(response)
-          this.$emit('get-all-items')
-        })
-        .then(() => {
-          this.initializeTempItem()
-          this.$emit('posted-item')
-        })
-        .catch((response) => console.log(response))
-    },
-    validate(): void {
-      const formRef = this.$refs.form as any
-      formRef.validate()
-    },
-  },
-})
-</script>
